698a15ccbf298882c284d42a9090d86b18b6f4dd
Alejandro Gonzalez <alejandrogxv@gmail.com>
2018-08-11 23:21:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,10 @@ function logShout(string) {
 function logWhisper(string){
   console.log('hello')
 }
+
+function sayHiToGrandma(string) {
+  var lowercase = 'hello!'
+  lowercase.toLowerCase() ===
+  lowercase
+  return 'I can\'t hear you!'  
+}
